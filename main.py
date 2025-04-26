0c078cf1dc912825e3cc5b63be53ed87d592747c
Adarsh Kumar <nextgen.adarsh@outlook.com>
2025-04-26 20:28:22 +0530

    Add agent and parsers

--- main.py
+++ main.py
@@ -5,29 +5,12 @@ from langchain_ollama import ChatOllama
 
 from dotenv import load_dotenv
 
-from src.linkedin import scrape_linkedin_profile
-
 if __name__ == "__main__":
     print("Welcome to Langchain !!")
 
     load_dotenv()
 
-    summary_template = """
-        given the information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+import src.demo_001_langchain
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
+# import src.agents.linkedin_lookup_agent
 
-    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-    # llm = ChatOllama(temperature=0, model="llama3")
-    llm = ChatOllama(temperature=0, model="mistral")
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        "http://linkedin.com/in/nextgenadarsh/", True
-    )
-    res = chain.invoke(input={"information": linkedin_data})
-    print(res)
